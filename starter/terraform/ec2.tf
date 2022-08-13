f3e06dcde296f9ee6eace83c46d88101b083c06a
ngoctth <hongngoc219.90@gmail.com>
2022-08-13 18:36:28 +0700

    project 1

--- starter/terraform/ec2.tf
+++ starter/terraform/ec2.tf
@@ -6,12 +6,14 @@
 #    private_subnet_ids = module.vpc.private_subnet_ids
 #    vpc_id             = module.vpc.vpc_id
 #  }
+#us-east-1: ami-0252248182aeb2136
+#us-east-2: ami-05d388235e3b33205
 
   module "project_ec2" {
    source             = "./modules/ec2"
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
-   aws_ami            = "ami-0ef8c7099ef697f3e"
+   aws_ami            = "ami-05d388235e3b33205"
    private_subnet_ids = module.vpc.private_subnet_ids
    vpc_id             = module.vpc.vpc_id
  }
--- starter/terraform/eks.tf
+++ starter/terraform/eks.tf
@@ -18,5 +18,13 @@ provider "kubernetes" {
    name               = local.name
    account            = data.aws_caller_identity.current.account_id
    private_subnet_ids = module.vpc.private_subnet_ids
+   ec2_sg             = module.project_ec2.ec2_sg
    vpc_id             = module.vpc.vpc_id
+   nodes_desired_size = 1
+   nodes_max_size     = 2
+   nodes_min_size     = 1
+
+   depends_on = [
+    module.vpc,
+   ]
  }
